983089b4ca8d362d43a8a64d164ac4df6bc78376
Công Sơn <sonnguyen1129@gmail.com>
2022-01-08 17:03:47 +0700

    init project

---
+++ src/main/java/com/example/weatherbot/WeatherbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherbotApplication.class, args);
+	}
+
+}
